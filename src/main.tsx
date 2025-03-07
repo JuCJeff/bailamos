065303519b140688c027b7e538dbba97aa25484f
Jeff Ma <jucjeff96@gmail.com>
2025-03-07 16:30:54 -0600

    refactor: change theme to use tailwind and shadcn

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { Theme } from "@radix-ui/themes";
-import "@radix-ui/themes/styles.css";
+import { ThemeProvider } from "@/components/theme-provider";
 
 import App from "./App.tsx";
 
+import "./index.css";
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <Theme appearance="dark">
+    <ThemeProvider defaultTheme="system" storageKey="vite-ui-theme">
       <App />
-    </Theme>
+    </ThemeProvider>
   </StrictMode>
 );
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
   base: "/bailamos/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });

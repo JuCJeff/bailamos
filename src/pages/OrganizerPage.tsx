20fa9f770e447962567166ca21f32c691aad1c41
Jeff Ma <jucjeff96@gmail.com>
2025-08-14 14:58:35 -0500

    fix: remove unused imports

--- src/pages/OrganizerPage.tsx
+++ src/pages/OrganizerPage.tsx
@@ -1,11 +1,8 @@
-import { useAuth } from "@/contexts";
 import { HeadBar } from "@/components/layout";
 import { LogOutButton } from "@/components/auth";
 import { Dashboard } from "@/components/organizer";
 
 export default function OrganizerPage() {
-  const { user, organizerProfile } = useAuth();
-
   return (
     <div>
       <HeadBar />
